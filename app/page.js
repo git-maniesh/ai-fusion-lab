d98a800e4a478e4b6ac66ef64de63041438cf03a
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-10-03 08:20:26 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -5,12 +5,14 @@ import { Button } from "@/components/ui/button";
 import { useTheme } from "next-themes";
 import Image from "next/image";
 import ChatInputBox from "./_components/ChatInputBox";
+import { Suspense } from "react";
 
 export default function Home() {
   const {setTheme} = useTheme()
   return (
-    <div>
-     <ChatInputBox />
-    </div>
+    <Suspense fallback={<div>Loading chat...</div>}>
+      <ChatInputBox />
+    </Suspense>
+
   );
 }
