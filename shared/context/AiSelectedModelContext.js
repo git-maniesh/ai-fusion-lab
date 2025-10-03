e1bd177b05d8e770d62e93d2bdc82ac0ed1e40c8
maniesh_m <64514465+git-maniesh@users.noreply.github.com>
2025-10-03 09:36:26 +0530

    Update AiSelectedModelContext.js

--- shared/context/AiSelectedModelContext.js
+++ shared/context/AiSelectedModelContext.js
@@ -1,5 +1,24 @@
-import { createContext } from "react";
 
 
+"use client";
 
-export const AiSelectedModelContext = createContext()
+import React, { createContext, useState } from "react";
+
+export const AiSelectedModelContext = createContext();
+
+export const AiSelectedModelProvider = ({ children }) => {
+  const [aiSelectedModels, setAiSelectedModels] = useState({
+    GPT: { modelId: "gpt-3.5", enable: true },
+    Claude: { modelId: "claude-1", enable: false },
+  });
+
+  const [messages, setMessages] = useState({}); // { GPT: [...], Claude: [...] }
+
+  return (
+    <AiSelectedModelContext.Provider
+      value={{ aiSelectedModels, setAiSelectedModels, messages, setMessages }}
+    >
+      {children}
+    </AiSelectedModelContext.Provider>
+  );
+};
